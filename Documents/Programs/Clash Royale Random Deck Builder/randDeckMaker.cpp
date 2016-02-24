6aef0508abb1f99cf5f09513adb58cec33632c4d
Luke Justice <lmjustic@umich.edu>
2016-02-23 20:13:03 -0500

    added the ability to remove cards from the random pool

--- Documents/Programs/Clash Royale Random Deck Builder/randDeckMaker.cpp
+++ Documents/Programs/Clash Royale Random Deck Builder/randDeckMaker.cpp
@@ -11,12 +11,14 @@
 #include <ctime>
 using namespace std;
 
-bool newElt(int arr[], int size, int elt);
+int getIndex(const string arr[], int size, string card);
+bool inArray(int arr[], int size, int card);
 void printDeck(int deck[], int deckSize, const string cardList[]);
 
 int main() {
 	const int NUM_CARDS = 42;
 	const int DECK_SIZE = 8;
+	const int MAX_BAN_SIZE = NUM_CARDS - DECK_SIZE;
 	const string CARD_LIST[NUM_CARDS] = {"Arrows", "Bomber", "Archers", "Knight",
 																			 "Fireball", "Mini P.E.K.K.A", 
 																			 "Musketeer", "Giant", "Prince", 
@@ -32,41 +34,91 @@ int main() {
 																			 "Hog Rider", "Freeze", "P.E.K.K.A",
 																			 "Zap", "Wizard", "Mirror", "Mortar",
 																			 "Elixir Collector", "Golem"};
-	int randDeck[DECK_SIZE] = { -1, -1, -1, -1, -1, -1, -1, -1 };
+	int randDeck[DECK_SIZE];
+	int bannedCards[NUM_CARDS];
+	int numBans;
 	int randCard;
+	int banID;
 	bool newRandCard;
-	string end = "";
+	bool isBannedCard;
+	bool newBan;
+	string quit = "";
+	string ban;
 	srand(time(0));
 	
+	// Controls the removal of cards from the total pool
 	cout << "Thank you for using the Random Deck Builder\n";
-	while (end != "quit") {
+	while (quit != "quit") {
+		ban = "";
+		numBans = 0;
+		while (ban != "done" && numBans < MAX_BAN_SIZE) {
+			cout << "\nEnter the name of a card to remove from the random pool.\n";
+			cout << "Type \"done\" to finish your removals and generate a deck.\n";
+			getline(cin, ban);
+			banID = getIndex(CARD_LIST, NUM_CARDS, ban);
+			newBan = !inArray(bannedCards, numBans, banID);
+			if (banID < 0) {
+				cout << ban << " is not a valid card. Type the name exactly as it ";
+				cout << "appears on the in-game card.\n";
+			}
+			else if (banID >= 0 && newBan) {
+				bannedCards[numBans] = banID;
+				numBans++;
+				cout << ban << " was successfully removed.\n";
+				if (numBans >= MAX_BAN_SIZE) {
+					cout << "Max ban size reached.\n";
+				}
+			}
+			else {
+				cout << "Card was previously removed from the pool.\n";
+			}
+		}
+		
+		// Generates the random deck
 		for (int i = 0; i < DECK_SIZE; i++) {
 			newRandCard = false;
-			while (!newRandCard) {
+			isBannedCard = true;
+			while (!newRandCard || isBannedCard) {
 				randCard = rand() % 42;
-				newRandCard = newElt(randDeck, DECK_SIZE, randCard);
+				newRandCard = !inArray(randDeck, DECK_SIZE, randCard);
+				isBannedCard = inArray(bannedCards, numBans, randCard);
 			}
 			randDeck[i] = randCard;
 		}
-		cout << "Here is your random deck\n";
+		
+		// Controls the loop of the program
+		cout << "Here is your random deck.\n";
 		printDeck(randDeck, DECK_SIZE, CARD_LIST);
-		cout << "Would you like to generate a new deck? Type \"quit\" to quit\n";
-		cin >> end;
+		cout << "Would you like to generate a new deck? Type \"quit\" to quit.\n";
+		getline(cin, quit);
 	}
 	
 	return 0;
 }
 
-// REQUIRES	size >= 0 and size < the size of arr[]
-// EFFECTS	returns true if elt is not in arr[];
-//					otherwise returns false
-bool newElt(int arr[], int size, int elt) {
+// REQUIRES	size >= 0 and size < the size of arr[]; arr[] is not empty;
+//					card is not null
+// EFFECTS	returns the first index of arr[] if card appears within arr[]
+//					otherwise, returns -1
+int getIndex(const string arr[], int size, string card) {
 	for (int i = 0; i < size; i++) {
-		if (elt == arr[i]) {
-			return false;
+		if (card == arr[i]) {
+			return i;
 		}
 	}
-	return true;
+	return -1;
+}
+
+// REQUIRES	size >= 0 and size < the size of arr[]; arr[] is not empty
+// EFFECTS	returns true if card is in arr[];
+//					otherwise, returns false
+bool inArray(int arr[], int size, int card) {
+	for (int i = 0; i < size; i++) {
+		if (card == arr[i]) {
+			return true;
+		}
+	}
+	return false;
 }
 
 // REQUIRES	All elements in deck[] are >= 0 and < the size of cardList[];
